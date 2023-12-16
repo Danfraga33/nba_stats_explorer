095c28c065462cd08c605e9d23f55dda288ca87a
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-16 23:57:23 +0000

    style: import custom Helvetica Font and applying

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,10 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { Roboto } from 'next/font/google';
-
-const roboto = Roboto({ weight: ['400', '500'], subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<main className={roboto.className}>
+		<div>
 			<Component {...pageProps} />;
-		</main>
+		</div>
 	);
 }
