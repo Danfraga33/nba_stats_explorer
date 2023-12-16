bead3a1a2cd4baebc3633741195ad2232354b63b
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-16 23:58:26 +0000

    fix: fixing responsive viewport

--- components/MainContent.tsx
+++ components/MainContent.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react';
 
 const MainContent: FC = () => {
 	return (
-		<main className="container mt-4 md:flex  flex-row-reverse justify-evenly items-center bg-blue-500">
+		<main className="container mt-4 flex  flex-row-reverse justify-evenly items-center bg-blue-500">
 			Main Content
 		</main>
 	);
