e0270ad10e38ca0b1aa83f4f50a4ef2d3d26ca5f
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-16 23:56:22 +0000

    feat:adding in basketball logo from font-awesome

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -1,12 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBasketballBall } from '@fortawesome/free-solid-svg-icons';
 import Link from 'next/link';
 import React, { FC } from 'react';
 
 const Navigation: FC = () => {
 	return (
 		<header className="container px-2 flex items-center justify-between shadow-md md:shadow-none h-20 bg-red-500">
-			<div className="text-2xl">NBA Stats Explorer</div>
-			<nav>
-				<ul className="hidden md:flex items-center space-x-3 lg:space-x-8 ">
+			<FontAwesomeIcon icon={faBasketballBall} className="text-4xl pr-2" />
+			<div className="hidden md:block text-2xl">NBA Stats Explorer</div>
+
+			<nav className="w-full justify-evenly md:justify-end flex">
+				<ul className=" items-center space-x-3 lg:space-x-8 flex ">
 					<li>
 						<Link href="/">All Teams</Link>
 					</li>
