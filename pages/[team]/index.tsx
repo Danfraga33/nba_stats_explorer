6b2a66039f86406d7791e384486e5dba2ff8a03c
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-17 17:18:03 +0000

    feat: connecting api + showing team data

---
+++ pages/[team]/index.tsx
@@ -0,0 +1,20 @@
+// import { NextPage } from 'next';
+// import React from 'react';
+
+// export interface TeamProps {
+// 	team: Array<{
+// 		id: number;
+// 		abbreviation: string;
+// 		city: string;
+// 		conference: string;
+// 		division: string;
+// 		full_name: string;
+// 		name: string;
+// 	}>;
+// }
+
+// const Team: NextPage = ({ team }): TeamProps => {
+// 	return <div>index</div>;
+// };
+
+// export default Team;
