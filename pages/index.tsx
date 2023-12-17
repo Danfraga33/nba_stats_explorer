42e3277522d52557829ebb959de183e349e33324
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-17 17:17:41 +0000

    feat: creating home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,47 @@ import Header from '@/components/Header';
 import MainContent from '@/components/MainContent';
 import Navigation from '@/components/Navigation';
 import { NextPage } from 'next';
+import React, { useState, useEffect, ReactElement } from 'react';
+import Layout from '@/Layout';
+import { NextPageWithLayout } from './_app';
 
-const Home: NextPage = () => {
+const Home: NextPageWithLayout = () => {
+	const [teams, setTeams] = useState();
+	useEffect(() => {
+		async function basketballPlayers() {
+			try {
+				// Data
+				const response = await fetch('api/teams');
+				if (!response.ok) {
+					throw new Error('Failed to fetch data');
+				}
+				// Parse JSON response
+				const result = await response.json();
+				const nbaTeamsData = result.data;
+				setTeams(nbaTeamsData);
+				// console.log(nbaTeamsData);
+			} catch (err: any) {
+				console.error('Error', err.message);
+			}
+		}
+		basketballPlayers();
+	}, []);
+
 	return (
 		<>
 			<div className="px-10 py-1">
 				<Header />
-				<Navigation />
-				<MainContent />
+				<MainContent teams={teams} />
 			</div>
 		</>
+	);
+};
+
+Home.getLayout = function getLayout(page: ReactElement) {
+	return (
+		<div>
+			<Layout>{page}</Layout>
+		</div>
 	);
 };
 
