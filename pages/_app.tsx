97b29aefb3088f22c1b1472f21b3fdfdd29e318b
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-17 17:17:27 +0000

    feat: creating layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,20 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { NextPage } from 'next';
+import React, { FC, ReactNode, ReactElement } from 'react';
+import Layout from '@/Layout';
 
-export default function App({ Component, pageProps }: AppProps) {
-	return (
-		<div>
-			<Component {...pageProps} />;
-		</div>
-	);
-}
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+	getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+	Component: NextPageWithLayout;
+};
+
+const App: FC<AppProps> = ({ Component, pageProps }: AppPropsWithLayout) => {
+	const getLayout = Component.getLayout ?? ((page) => page);
+
+	return getLayout(<Component {...pageProps} />);
+};
+export default App;
