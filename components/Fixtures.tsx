e3f449a45652ff46c25ae6f3c9b2c80a890838fd
Daniel Fraga <dan.fraga33@gmail.com>
2023-12-20 11:45:01 +0000

    style: install material UI

--- components/Fixtures.tsx
+++ components/Fixtures.tsx
@@ -1,5 +1,10 @@
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
 import gameData from '../games.json';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+
 export interface FixturesDataProps {
 	fixturesData?: {
 		id: number;
@@ -17,10 +22,26 @@ export interface FixturesDataProps {
 }
 
 const Fixtures: FC<FixturesDataProps> = ({ fixturesData }) => {
+	const [open, setOpen] = useState(false);
+	const handleOpen = () => setOpen(true);
+	const handleClose = () => setOpen(false);
+
 	if (!fixturesData) {
 		return <p>Loading...</p>;
 	}
-
+	const style = {
+		position: 'absolute' as 'absolute',
+		top: '50%',
+		left: '50%',
+		transform: 'translate(-50%, -50%)',
+		width: 400,
+		bgcolor: '#fff',
+		border: '2px solid #000',
+		boxShadow: 24,
+		p: 4,
+		color: '#fff',
+	};
+	console.log(fixturesData);
 	return (
 		<>
 			<h1 className="flex items-center justify-center text-xl font-bold p-2 bg-white rounded-lg mt-2">
@@ -45,6 +66,42 @@ const Fixtures: FC<FixturesDataProps> = ({ fixturesData }) => {
 									<div>{game.visitor_team.full_name}</div>
 									<div>{game.visitor_team_score}</div>
 								</div>
+								<div></div>
+							</div>
+
+							<div>
+								<Button onClick={handleOpen}>Explore</Button>
+								<Modal
+									open={open}
+									onClose={handleClose}
+									aria-labelledby="modal-modal-title"
+									aria-describedby="modal-modal-description"
+								>
+									<Box sx={style}>
+										<Typography
+											id="modal-modal-title"
+											variant="h6"
+											component="h2"
+										>
+											{game.home_team.full_name}
+										</Typography>
+										<Typography
+											id="modal-modal-description"
+											sx={{ mt: 2, flex: 'flex-col' }}
+										>
+											<div className="block">
+												Points: {game.home_team_score}
+											</div>
+											{/* <div className="block">
+												Postseason: {game.postseason === false ? 'No' : 'Yes'}
+											</div>
+											<div className="block">
+												Final:
+												{game.status === 'Final' ? 'Final' : 'Season Game'}
+											</div> */}
+										</Typography>
+									</Box>
+								</Modal>
 							</div>
 						</li>
 					</ul>
